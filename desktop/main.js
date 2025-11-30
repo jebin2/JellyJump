b68f918c806980f0e87c4f3148791e1783cefbe2
Jebin Einstein <jebineinstein@gmail.com>
2025-11-30 19:40:40 +0530

    electron desktop app

---
+++ desktop/main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+            webSecurity: true // Important for loading local resources properly
+        },
+        autoHideMenuBar: true,
+        title: "MediaBunny Player"
+    });
+
+    // Load the index.html from the build folder
+    // In development, we might want to load from ../build, but for packaging, 
+    // we will copy build to ./build
+    const buildPath = path.join(__dirname, 'build/index.html');
+
+    win.loadFile(buildPath).catch(err => {
+        console.error('Failed to load index.html:', err);
+        // Fallback for dev if not copied yet
+        win.loadFile(path.join(__dirname, '../build/index.html'));
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
