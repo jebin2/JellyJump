23b88269f5cd46d9ffe1eb55de61282a2a196839
jebin2 <jebineinstein@gmail.com>
2026-01-04 23:38:21 +0530

    Comment out play overlay hiding and one-time unmute event listeners.

--- assets/js/core/Player.js
+++ assets/js/core/Player.js
@@ -3051,20 +3051,20 @@ export class CorePlayer {
                         await this.streamVideo.play();
                         this.isPlaying = true;
                         this._updatePlayPauseUI();
-                        if (this.ui.playOverlay) {
-                            this.ui.playOverlay.style.display = 'none';
-                        }
+                        // if (this.ui.playOverlay) {
+                        //     this.ui.playOverlay.style.display = 'none';
+                        // }
                         // Set up one-time click handler to unmute
-                        const unmuteHandler = () => {
-                            if (this.streamVideo && this.isPlaying) {
-                                this.streamVideo.muted = this.config.muted || false;
-                                console.log('[Stream] Unmuted after user interaction');
-                            }
-                            document.removeEventListener('click', unmuteHandler);
-                            document.removeEventListener('touchstart', unmuteHandler);
-                        };
-                        document.addEventListener('click', unmuteHandler, { once: true });
-                        document.addEventListener('touchstart', unmuteHandler, { once: true });
+                        // const unmuteHandler = () => {
+                        //     if (this.streamVideo && this.isPlaying) {
+                        //         this.streamVideo.muted = this.config.muted || false;
+                        //         console.log('[Stream] Unmuted after user interaction');
+                        //     }
+                        //     document.removeEventListener('click', unmuteHandler);
+                        //     document.removeEventListener('touchstart', unmuteHandler);
+                        // };
+                        // document.addEventListener('click', unmuteHandler, { once: true });
+                        // document.addEventListener('touchstart', unmuteHandler, { once: true });
                         console.log('[Stream] Playing muted (touch/click to unmute)');
                     } catch (mutedError) {
                         console.error('[Stream] Even muted play failed:', mutedError);
